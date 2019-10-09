e2a11564241e060e4f27dd73cc07a95cc0e07775
yeo040294 <you@example.com>
2019-10-09 12:29:46 +0800

    first page second draft

--- mini.py
+++ mini.py
@@ -1,15 +1,28 @@
 from tkinter import *
-from datetime import datetime
-
-
-
-
+import time
 
 window = Tk()
-window.title("CZ1003 Mini Project")
-window.geometry("500x400")
 
-time = StringVar()
+time1 = ''
+clock = Label(window, font=('times', 20, 'bold'), bg= 'green')
+clock.pack(fill=BOTH, expand=1)
+def tick():
+    global time1
+    # get the current local time from the PC
+    time2 = time.strftime('%a, %d %b %y %H:%M:%S')
+    # if time string has changed, update it
+    if time2 != time1:
+        time1 = time2
+        clock.config(text=time2)
+    # calls itself every 200 milliseconds
+    # to update the time display as needed
+    # could use >200 ms, but display gets jerky
+    clock.after(200, tick)
+tick()
+
+window.title("CZ1003 Mini Project")
+window.geometry("500x430")
+
 mainpage= StringVar()
 Mainpage = Label( window, textvariable=mainpage, font = ("arial 15 bold italic"), relief=RAISED )
 mainpage.set("Welcome to NTU NorthSpine Food Section")
@@ -19,14 +32,13 @@ logo = PhotoImage(file= "canteen.ppm")
 
 
 
-currenttime = Label( window, textvariable=time, font = ("arial 10 bold italic"))
-time.set(datetime.now().strftime("%a, %d %b %Y %H:%M:%S"))
+
 
 
 buttontoday = Button(window, text="View Today Menu")
 buttonanotherday = Button(window, text="View Other Day Menu")
 Mainpage.pack()
-currenttime.pack()
+
 
 
 frame = Frame(window, width= 400,height = 300)
