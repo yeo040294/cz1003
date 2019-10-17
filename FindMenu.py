baad71c15b80e904bbfafe426d7ad554207bde47
ycheah003 <56013527+ycheah003@users.noreply.github.com>
2019-10-17 14:33:30 +0800

    Add files via upload

---
+++ FindMenu.py
@@ -0,0 +1,23 @@
+def BreakfastMenu(day,stall): #Find breakfast menu for stall on specified day
+    import csv
+    from datetime import datetime
+    from collections import defaultdict
+    with open("Menu.csv") as file_pointer:
+        csv_pointer = csv.reader(file_pointer)
+        newdict = {}
+        for each in csv_pointer:
+            newdict.setdefault(each[0], {})[each[1]] = each[2]
+        query = newdict[day][stall]
+    return query
+
+def LunchDinnerMenu(day,stall): #Find lunch and dinner menu for stall on specified day
+    import csv
+    from datetime import datetime
+    from collections import defaultdict
+    with open("Menu.csv") as file_pointer:
+        csv_pointer = csv.reader(file_pointer)
+        newdict = {}
+        for each in csv_pointer:
+            newdict.setdefault(each[0], {})[each[1]] = each[3]
+        query = newdict[day][stall]
+    return query
