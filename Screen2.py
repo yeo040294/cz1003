d7e532066e1b47bab5ac957810cc9b0c7426af5d
yeo040294 <you@example.com>
2019-11-05 23:21:23 +0800

    changes

--- Screen2.py
+++ Screen2.py
@@ -7,7 +7,6 @@ from tkinter import *
 import Screen3 as scr3
 from functools import partial
 
-<<<<<<< HEAD
 def screen():
 
     window = Tk()
@@ -17,10 +16,10 @@ def screen():
 
     #Formatting
     time, subpage2 = StringVar(), StringVar()
-    Subpage2 = Label(window, textvariable=subpage2, font = ("arial 15 bold italic")).pack()
-    subpage2.set("Select a store from the following options")
-    currenttime = Label(window, textvariable=time, font = ("arial 10 bold italic")).pack()
-    time.set(datetime.now().strftime("%a, %d %b %Y %H:%M:%S"))
+    Subpage2 = Label(window, text= "Select a store from the following options" , font = ("arial 15 bold italic")).pack()
+    
+    currenttime = Label(window, text = datetime.now().strftime("%a, %d %b %Y %H:%M:%S"), font = ("arial 10 bold italic")).pack()
+    
 
     btn_list =[]
 
@@ -39,30 +38,3 @@ def screen():
         
 
     window.mainloop()
-=======
- 
-#root Size
-window.title("Choose Store to Query")
-window.geometry("500x400")
-
-#Formatting
-time, subpage2 = StringVar(), StringVar()
-Subpage2 = Label(window, textvariable=subpage2, font = ("arial 15 bold italic")).pack()
-subpage2.set("Select a store from the following options")
-currenttime = Label(window, textvariable=time, font = ("arial 10 bold italic")).pack()
-time.set(datetime.now().strftime("%a, %d %b %Y %H:%M:%S"))
-
-btn_list =[]
-    
-def openScreen3(stallName):
-    scr3.create(stallName)
-
-#Creating buttons
-for i in StallList(1):
-    if i == "":
-        pass
-    else:
-        Button(window, text=i , command=partial(openScreen3,i)).pack(side="bottom", padx=10, pady=10)
-
-window.mainloop()
->>>>>>> dbe95aed250c6a455a16e3a44ec2ee3791d5c403
--- miniprojectupdated.py
+++ miniprojectupdated.py
@@ -20,6 +20,7 @@ def tick():
     if time2 != time1:
         time1 = time2
         clock.config(text=time2)
+    
     # calls itself every 200 milliseconds
     # to update the time display as needed
     # could use >200 ms, but display gets jerky
